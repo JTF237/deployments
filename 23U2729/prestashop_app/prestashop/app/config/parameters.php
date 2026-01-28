5e58485e08a52e7b0146f22e0206cbb97b14d162
JUNIOR TEMDEMNOU FABIEN <Jtf@vmi2924532.contaboserver.net>
2026-01-28 11:57:21 +0100

    prestashop_app

---
+++ 23U2729/prestashop_app/prestashop/app/config/parameters.php
@@ -0,0 +1,64 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'db',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'prestashop',
+    'database_password' => 'prestashop123',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'bpPmhDfEAmIE0HtQ4Ow7LYq3rQZ6jqFkAWtX7v5mZef9wTzOIIRhTXXELExGJrPe',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2026-01-27',
+    'locale' => 'fr-CA',
+    'use_debug_toolbar' => true,
+    'cookie_key' => '8CYh7PNpuxSKnMssbB6o2s5QUOFNYEFOYeNQSYeKenv0nz3oiFFPaHQWY3sPErGn',
+    'cookie_iv' => 'wFHyOeeQKGrwRDKRLWyfigRasbHgLZw3',
+    'new_cookie_key' => 'def000007d009d316d9759a392e85f3c398b7882b0b0774f28dc62582101862ced7efd5f035cc72799a9dbe9e915b57d05afc026fa6d02494a2c48538dd04414c60bcf51',
+    'api_public_key' => '-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsEGkQA2XkO+EiQ1lfBqB
+wK9AwwDZ1erFVUKwQR4S9TbMbnCrJeOglmJNth2Qzv1S9pXazFoDTzIgwOWJK28h
+hvaavY7rm9RntScHBO2HgUwc/OQ2ZR/7jVbfOUtHSmx8OiBY1k/V9G4OEpL98Wbs
+JZ/3dVhjyvQUepHvYaL1MDHVPGCleRXx2WpunUiZH0vyVvkaenXOKhIv84mjFtzd
+mgRO6UaMi0sPiKKSkNxMT7Duk1CLrTc9RUt7lCZ22rSgpzEA49HE5gUgdPDUpkHz
+fmA8ne6/PHLAzx2e1kzeEVT5hG/ahPUglZXk1F+XPyPPDvTHQ8o9MFjN8XvmF01z
+0wIDAQAB
+-----END PUBLIC KEY-----
+',
+    'api_private_key' => '-----BEGIN PRIVATE KEY-----
+MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCwQaRADZeQ74SJ
+DWV8GoHAr0DDANnV6sVVQrBBHhL1NsxucKsl46CWYk22HZDO/VL2ldrMWgNPMiDA
+5YkrbyGG9pq9juub1Ge1JwcE7YeBTBz85DZlH/uNVt85S0dKbHw6IFjWT9X0bg4S
+kv3xZuwln/d1WGPK9BR6ke9hovUwMdU8YKV5FfHZam6dSJkfS/JW+Rp6dc4qEi/z
+iaMW3N2aBE7pRoyLSw+IopKQ3ExPsO6TUIutNz1FS3uUJnbatKCnMQDj0cTmBSB0
+8NSmQfN+YDyd7r88csDPHZ7WTN4RVPmEb9qE9SCVleTUX5c/I88O9MdDyj0wWM3x
+e+YXTXPTAgMBAAECggEAJjh9ySQJZ3UBDQU6KhDHbUSzqvWuxywLUaNN/DjszH7v
+Es3c1zEGvEWh62l64t/dEzrIwQKN8CkDIEFGHwSQxScJxCxPXDHRsV8+ruthNIEQ
+UiicwQ4P6wTZECAKXBGtWJ92FDcowsHwzEdSyPvUHo6TRz99GGAYZsHpoocHW0U+
+TEuUy2skwqh8FXL3A7ZyYKrckl20r8Sr8S6iRpApd3vWyavBgFSAhYPT4KClJ1HM
+cSLeTFoUzUX2KI4B21zSTOVK9oRl4y9rtuCs3/k4NUlnBDYrdfU1KDor7mduedwi
+Vd90KifmO+tFy8ZhB/Zq0EO4rK61EbafEmKyMTS/YQKBgQDwmKSTn7YNicjtubqI
+aDGd8OlKEMdXxg3IL4taBhz5Oe9MNd0GU6HS2gZCi35cZU/Dd2cOjiAyIdD+qFC6
+pZHywFTnchfOe/T6NOaLxcA4m5PmBQzv7djgl8fZJ2En2YPJRBThOzwFzTUH36wH
+Pd465S55REof/CEFJhI2P6BCoQKBgQC7inkH0xbFgJ1IsA2hDgvM04/gmllkcxsr
+s7Las6EmwLOPqnoH13NTN31QKncrTEZ1Y6L1L98wi+FkS98PDeqsEcsfx4lfZV1w
+QOpzO7UzV7aeEGPK7Wx9EhFqtE8tChQkNY0PU1vzCf4ixEJ+qSZ5LPHDd2IJLu1v
+6lr36FcV8wKBgEMPmNfY7WqKnCm+2gJJYIMQckYFtTp+5rqEngfiqlLBVjTc0pV/
+3BwkTxhfP0JLrtbhwiTpconmqRVCHvZXTf3mpAO65m3UWS0gb55H6VMd8jRr9fYu
+SoQXaknZ0xfDZhtEUbmRxCIt13kxXAbMtNOfq7t7UFnn8eA4g10BkQ7hAoGAZmcJ
+CT243GUK/Oqstnld9hy7HjAJibDUcDtx+LtGzktAgLzcXzXEkfblpQVbBrhibKzY
+CTt3iiGHE+XdyXGW5xfcKuEgYIePysCXhA5hNcS6QXq/ksmriGt6OSJ1bSlosBDR
+kkG5Hm8hZkh16oVgLaBd7pZcYJlQKuDzhRRHoQMCgYBUupB86u/J/xlY5KQF+SKx
+IH2TumYom7BGv7UixteF/MOh2GzmdXcmVk3T6cLxF0/zfdKiAlu9QSpFn00ACtOc
+hDMqucdJ3EN99U+QNOUQfv5LuHuy7iNWhTKGtzB3m8KPA9iAjq9tKZpyPzXhGngI
+Wt7WzQ/1YsgkeI/WdUE/cA==
+-----END PRIVATE KEY-----
+',
+  ),
+);
